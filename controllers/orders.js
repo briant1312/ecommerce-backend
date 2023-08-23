fbaae5a1129496e4f6cb66c47da8a6b5d1f063a5
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-08-23 16:28:18 -0400

    update order controllers

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,12 +1,19 @@
 const db = require("../config/database");
 
-async function create(req, res) {
+async function getUserCart(req, res) {
     const userId = req.user.id;
+
     try {
-        const order = await db.oneOrNone(
-            'INSERT INTO orders (user_id) VALUES ($1) RETURNING *',
+        let order = await db.oneOrNone(
+            'SELECT id FROM orders WHERE (user_id=$1 AND is_completed=false)',
             [userId]
         )
+        if (!order) {
+            order = await db.one(
+                'INSERT INTO orders (user_id) VALUES ($1) RETURNING id',
+                [userId]
+            )
+        }
         res.json(order);
     } catch (error) {
         // res.status(400).json("error creating order");
@@ -14,13 +21,15 @@ async function create(req, res) {
     }
 }
 
+// TODO: verify qty in stock and update after completing the order
 async function completeOrder(req, res) {
-    const id = req.params.id;
+    const orderId = req.params.orderId;
     const userId = req.user.id;
+
     try {
         const order = await db.oneOrNone(
             'UPDATE orders SET is_completed = true WHERE (id=$1 AND user_id=$2) RETURNING *',
-            [id, userId]
+            [orderId, userId]
         )
         res.json(order);
     } catch (error) {
@@ -30,43 +39,95 @@ async function completeOrder(req, res) {
 }
 
 async function addItemToOrder(req, res) {
-    const itemId = req.params.itemId;
     const userId = req.user.id;
-    db.task(t => {
-        return t.oneOrNone('SELECT id FROM orders WHERE (user_id=$1 AND is_completed=false)', [userId])
-            .then(({ id: orderId }) => {
-                return t.none('INSERT into order_items (order_id, item_id) VALUES ($1, $2)', [orderId, itemId])
-            })
-    }) 
-        .then(() => {
+    const { itemId, orderId, qty } = req.body;
+
+    try {
+        const order = await db.one(
+            'SELECT is_completed, user_id FROM orders WHERE (id=$1)',
+            [orderId]
+        )
+        if (order.user_id !== userId) {
+            res.status(401).json("unauthorized");
+            return;
+        }
+        if (order.is_completed) {
+            res.status(400).json("cannot add items to completed orders");
+            return;
+        } 
+
+        const updatedField = await db.oneOrNone(
+            `UPDATE order_items SET
+            qty = qty + $3
+            WHERE (order_id=$2 AND item_id=$1) RETURNING *`,
+            [itemId, orderId, qty]
+        )
+
+        if (updatedField) {
             res.sendStatus(204);
-        })
-        .catch(error => {
-            // res.status(400).json("error creating order");
-            res.status(400).json(error.message);
-        })
+            return;
+        }
+
+        await db.none(
+            'INSERT INTO order_items (order_id, item_id, qty) VALUES ($1, $2, $3)',
+            [orderId, itemId, qty]
+        )
+        res.sendStatus(204);
+    } catch (error) {
+        // res.status(400).json("error creating order");
+        res.status(400).json(error.message);
+    }
 }
 
 async function removeItemFromOrder(req, res) {
-    const itemId = req.params.itemId;
     const userId = req.user.id;
-    db.task(t => {
-        return t.oneOrNone('SELECT id FROM orders WHERE (user_id=$1 AND is_completed=false)', [userId])
-            .then(({ id: orderId }) => {
-                return t.none('DELETE FROM order_items WHERE (order_id=$1 AND item_id=$2)', [orderId, itemId])
-            })
-    })
-        .then(() => {
+    const { itemId, orderId, qty } = req.body;
+
+    try {
+        const order = await db.one(
+            'SELECT is_completed, user_id FROM orders WHERE (id=$1)',
+            [orderId]
+        )
+        if (order.user_id !== userId) {
+            res.status(401).json("unauthorized");
+            return;
+        }
+        if (order.is_completed) {
+            res.status(400).json("cannot remove items from completed orders");
+            return;
+        } 
+
+        const orderItem = await db.oneOrNone(
+            'SELECT qty FROM order_items WHERE (order_id=$1 AND item_id=$2)',
+            [orderId, itemId]
+        )
+        if (!orderItem) {
             res.sendStatus(204);
-        })
-        .catch(error => {
-            // res.status(400).json("error creating order");
-            res.status(400).json(error.message);
-        })
+            return;
+        }
+        if (qty >= orderItem.qty) {
+            await db.none(
+                'DELETE FROM order_items WHERE (order_id=$1 AND item_id=$2)',
+                [orderId, itemId]
+            )
+            res.sendStatus(204);
+            return;
+        }
+        await db.none(
+            `UPDATE order_items SET
+            qty = qty - $3
+            WHERE (order_id=$2 AND item_id=$1)`,
+            [itemId, orderId, qty]
+        )
+        res.sendStatus(204);
+    } catch (error) {
+        // res.status(400).json("error creating order");
+        res.status(400).json(error.message);
+    }
 }
 
 module.exports = {
-    create,
+    getUserCart,
     completeOrder,
     addItemToOrder,
     removeItemFromOrder,
